9989b165e298cb9ca8b5d2c1089403ae9d977c71
Thomas Munzer <thomas.munzer@gmail.com>
2020-10-01 23:36:36 +0200

    adding psk creation/deletion

--- django_app/backend/urls.py
+++ django_app/backend/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('psks/', views.psks, name='psks'),
     path('psks/create/', views.createPsk, name='createPsks'),
+    path('psks/delete/', views.deletePsk, name='deletePsks'),
     path('wlans/', views.wlans, name='wlans'),
     path('login/', views.login, name='login'),
     path('sites/', views.sites, name='sites')    
