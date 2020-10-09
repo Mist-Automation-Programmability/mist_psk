de0d34eeb30e886cd36e3107386e3de659804d96
Thomas Munzer <thomas.munzer@gmail.com>
2020-10-09 08:38:13 +0200

    fix django conf

--- django_app/pskGen/settings.py
+++ django_app/pskGen/settings.py
@@ -124,9 +124,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static'),
+# )
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
