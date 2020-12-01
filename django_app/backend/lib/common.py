719ce80f52ebe9c8e5400575dba9374e77e1686c
Thomas Munzer <thomas.munzer@gmail.com>
2020-12-01 17:59:54 +0100

    fox EU and GC1 Cloud PSKs

--- django_app/backend/lib/common.py
+++ django_app/backend/lib/common.py
@@ -16,7 +16,12 @@ class Common:
         host = body["host"] if "host" in body else None
         # result
         #csrf
-        if body["cookies"] and "csrftoken" in body["cookies"]:
-            headers["X-CSRFToken"] = body["cookies"]["csrftoken"]
+        if body["cookies"] :
+            if "csrftoken" in body["cookies"]:
+                headers["X-CSRFToken"] = body["cookies"]["csrftoken"]
+            if "csrftoken.eu" in body["cookies"]:
+                headers["X-CSRFToken"] = body["cookies"]["csrftoken.eu"]
+            if "csrftoken.gc1" in body["cookies"]:
+                headers["X-CSRFToken"] = body["cookies"]["csrftoken.gc1"]
         extract = {"host": host, "headers": headers, "cookies": cookies }
         return extract
