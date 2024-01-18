d03ecfec02d9f369c5d8d128525d231dfa21e96f
Thomas Munzer <tmunzer@juniper.net>
2024-01-18 14:40:52 -0800

    adding build version in log

--- django_app/pskGen/views.py
+++ django_app/pskGen/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import redirect
 
+print("".center(80,"-"))
+print("Running Mist PSK - Build 240118.1.0")
+print("".center(80,"-"))
+
 def index(request):    
     return redirect("/")
 
