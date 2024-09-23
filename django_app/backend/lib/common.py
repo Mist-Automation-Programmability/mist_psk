a3de587926d572aaeddd53d371f7bc44887b4154
Thomas Munzer <tmunzer@juniper.net>
2024-09-23 11:38:49 +0200

    add missing clouds

--- django_app/backend/lib/common.py
+++ django_app/backend/lib/common.py
@@ -34,5 +34,15 @@ class Common:
                 headers["X-CSRFToken"] = body["cookies"]["csrftoken.eu"]
             if "csrftoken.gc1" in body["cookies"]:
                 headers["X-CSRFToken"] = body["cookies"]["csrftoken.gc1"]
+            if "csrftoken.gc2" in body["cookies"]:
+                headers["X-CSRFToken"] = body["cookies"]["csrftoken.gc2"]
+            if "csrftoken.gc3" in body["cookies"]:
+                headers["X-CSRFToken"] = body["cookies"]["csrftoken.gc3"]
+            if "csrftoken.ac2" in body["cookies"]:
+                headers["X-CSRFToken"] = body["cookies"]["csrftoken.ac2"]
+            if "csrftoken.ac5" in body["cookies"]:
+                headers["X-CSRFToken"] = body["cookies"]["csrftoken.ac5"]
+            if "csrftoken.ac6" in body["cookies"]:
+                headers["X-CSRFToken"] = body["cookies"]["csrftoken.ac6"]
         extract = {"host": host, "headers": headers, "cookies": cookies }
         return extract
